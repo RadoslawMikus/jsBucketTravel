e9b409cacd6fc4584e9b8b52c7887e692334d2af
davidsmith931 <radoslaw.mikus@gmail.com>
2022-04-14 16:14:33 +0200

    First commit

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  plugins: [new CleanWebpackPlugin()],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // 3. Creates a file with CSS
+          "css-loader", // 2. Turns CSS into JS
+          "sass-loader", // 1. Turns SCSS to CSS
+        ],
+      },
+
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  entry: {
+    main: "./src/main.js",
+    api: {
+      import: "./src/components/api.js",
+      filename: "components/[name].[hash].js",
+    },
+    list: {
+      import: "./src/components/list.js",
+      filename: "components/[name].[hash].js",
+    },
+    login: {
+      import: "./src/components/login.js",
+      filename: "components/[name].[hash].js",
+    },
+    modal: {
+      import: "./src/components/modal.js",
+      filename: "components/[name].[hash].js",
+    },
+    search: {
+      import: "./src/components/search.js",
+      filename: "components/[name].[hash].js",
+    },
+    zoom: {
+      import: "./src/components/zoom.js",
+      filename: "components/[name].[hash].js",
+    },
+  },
+
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/images/[name].[hash][ext]",
+  },
+
+  plugins: [new MiniCssExtractPlugin({ filename: "[name].[hash].css" })],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        inject: "body",
+        minify: {
+          removeAttributeQuotes: false,
+          collapseWhitespace: false,
+          removeComments: false,
+        },
+      }),
+    ],
+  },
+  devServer: {
+    hot: false,
+    liveReload: true,
+  },
+});
